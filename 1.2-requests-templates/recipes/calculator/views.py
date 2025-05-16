00186de2ef8a5ae0e1e407217f81c62c4045d28e
firerengoku <firerengoku@yandex.ru>
2025-05-16 13:28:05 +0300

    Second_HW

--- 1.2-requests-templates/recipes/calculator/views.py
+++ 1.2-requests-templates/recipes/calculator/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 DATA = {
@@ -16,15 +17,18 @@ DATA = {
         'сыр, ломтик': 1,
         'помидор, ломтик': 1,
     },
-    # можете добавить свои рецепты ;)
 }
 
-# Напишите ваш обработчик. Используйте DATA как источник данных
-# Результат - render(request, 'calculator/index.html', context)
-# В качестве контекста должен быть передан словарь с рецептом:
-# context = {
-#   'recipe': {
-#     'ингредиент1': количество1,
-#     'ингредиент2': количество2,
-#   }
-# }
+def recipe_view(request, dish):
+    servings = int(request.GET.get('servings', 1))
+    recipe = DATA.get(dish)
+    if not recipe:
+        return HttpResponse("Такого рецепта нет!")
+
+    scaled_recipe = {
+        ingredient: amount * servings
+        for ingredient, amount in recipe.items()
+    }
+
+    context = {'recipe': scaled_recipe}
+    return render(request, 'calculator/index.html', context)
