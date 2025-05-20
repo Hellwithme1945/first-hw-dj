a5fe23f3ab75268a0a6a704388f6401199cdeb36
firerengoku <firerengoku@yandex.ru>
2025-05-20 15:09:28 +0300

    Fourth_HW

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,30 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
+from .models import Article, Tag, Scope
 
-from .models import Article
+
+class ScopeInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        super().clean()
+        main_count = sum(1 for form in self.forms if form.cleaned_data.get('is_main') and not form.cleaned_data.get('DELETE', False))
+        if main_count == 0:
+            raise ValidationError('Укажите один основной раздел.')
+        elif main_count > 1:
+            raise ValidationError('Основным может быть только один раздел.')
+
+
+class ScopeInline(admin.TabularInline):
+    model = Scope
+    formset = ScopeInlineFormset
+    extra = 1
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    pass
+    inlines = [ScopeInline]
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['name']
